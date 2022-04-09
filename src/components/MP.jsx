604721731b653c2d42796f54ace7e5e7e7a7b38d
Natasha Nguyen <nngu0080@student.monash.edu>
2022-04-10 05:41:28 +1000

    Home, Nav, Footer, MP

---
+++ src/components/MP.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function MP(){
+    return (
+        <div class="px-4 py-5 my-5">
+        <div class="container col-lg-9 text-center">
+        <h1 class="lead mb-4 display-5 fw-bold">MP Name</h1>
+        <hr/>
+        </div>
+        <div class="col-lg-8 mx-auto">
+         
+          <table class="table table-bordered">
+            <tbody>
+                <tr>
+                    <td>Electorate</td>
+                    <td>All the Lorem Ipsum</td>
+                </tr>
+                <tr>
+                    <td>Party</td>
+                    <td>All the Lorem Ipsum</td>
+                </tr>
+                <tr>
+                    <td>Chamber</td>
+                    <td>All the Lorem Ipsum</td>
+                </tr>
+            </tbody>
+          </table>
+          
+
+          <h4 class=" fw-bold lh-1 mb-3 text-left">Policies</h4>
+        <p class="lead">In ultricies fermentum aliquet. Pellentesque dui magna, condimentum non ullamcorper at, cursus in sem. Nunc condimentum, purus ac enenatis commodo libero, am auctor porta tortor, nec consequat nibh finibus a. Sed ultrices risus eget iaculis luctus. Mauris vel gravida magna. </p>
+
+        <h4 class=" fw-bold lh-1 mb-3 text-left">Contact Details</h4>
+        <table class="table table-bordered">
+            <tbody>
+                <tr>
+                    <td>Email</td>
+                    <td>All the Lorem Ipsum</td>
+                </tr>
+                <tr>
+                    <td>Social Media</td>
+                    <td>All the Lorem Ipsum</td>
+                </tr>
+            </tbody>
+          </table>
+      </div>
+      </div>
+    );
+}
+
+export default MP;
